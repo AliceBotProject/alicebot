dfde8ff3f293cc8dc406d943ae81d942616bdafa
简律纯 <admin@jyunko.cn>
2023-04-07 16:58:42 +0800

    🛠修复链接

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
 
     editLink: {
       pattern:
-        "https://github.com/AliceBotProject/alicebot/edit/main/docs/:path",
+        "https://github.com/AliceBotProject/alicebot/edit/master/docs/:path",
       text: "在 GitHub 上编辑此页",
     },
 

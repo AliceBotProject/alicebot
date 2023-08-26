b868fca4c56b7083c1621f88cafa4fcb7dee1f4d
st1020 <stone_1020@qq.com>
2023-08-26 22:16:16 +0800

    fix: 修复 MyPy 报错

--- packages/alicebot-adapter-onebot/alicebot/adapter/onebot/message.py
+++ packages/alicebot-adapter-onebot/alicebot/adapter/onebot/message.py
@@ -55,49 +55,49 @@ class OneBotMessageSegment(MessageSegment["OneBotMessage"]):
         return f"[{self.type}: {self.data!r}]"
 
     @classmethod
-    def text(cls, text: str) -> "OneBotMessageSegment":
+    def text(cls, text: str) -> Self:
         """纯文本"""
         return cls(type="text", data={"text": text})
 
     @classmethod
-    def mention(cls, user_id: str) -> "OneBotMessageSegment":
+    def mention(cls, user_id: str) -> Self:
         """提及"""
         return cls(type="mention", data={"user_id": user_id})
 
     @classmethod
-    def mention_all(cls) -> "OneBotMessageSegment":
+    def mention_all(cls) -> Self:
         """提及所有人"""
         return cls(type="mention_all", data={})
 
     @classmethod
-    def image(cls, file_id: str) -> "OneBotMessageSegment":
+    def image(cls, file_id: str) -> Self:
         """图片"""
         return cls(type="image", data={"file_id": file_id})
 
     @classmethod
-    def voice(cls, file_id: str) -> "OneBotMessageSegment":
+    def voice(cls, file_id: str) -> Self:
         """语音"""
         return cls(type="voice", data={"file_id": file_id})
 
     @classmethod
-    def audio(cls, file_id: str) -> "OneBotMessageSegment":
+    def audio(cls, file_id: str) -> Self:
         """音频"""
         return cls(type="audio", data={"file_id": file_id})
 
     @classmethod
-    def video(cls, file_id: str) -> "OneBotMessageSegment":
+    def video(cls, file_id: str) -> Self:
         """视频"""
         return cls(type="video", data={"file_id": file_id})
 
     @classmethod
-    def file(cls, file_id: str) -> "OneBotMessageSegment":
+    def file(cls, file_id: str) -> Self:
         """文件"""
         return cls(type="file", data={"file_id": file_id})
 
     @classmethod
     def location(
         cls, latitude: float, longitude: float, title: str, content: str
-    ) -> "OneBotMessageSegment":
+    ) -> Self:
         """位置"""
         return cls(
             type="file",
@@ -110,6 +110,6 @@ class OneBotMessageSegment(MessageSegment["OneBotMessage"]):
         )
 
     @classmethod
-    def reply(cls, message_id: str, user_id: str) -> "OneBotMessageSegment":
+    def reply(cls, message_id: str, user_id: str) -> Self:
         """回复"""
         return cls(type="reply", data={"message_id": message_id, "user_id": user_id})

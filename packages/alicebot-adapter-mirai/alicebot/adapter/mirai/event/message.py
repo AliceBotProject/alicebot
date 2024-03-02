36249a9599e1036052d1451dd05d9669eabb7642
Stone <me@st1020.com>
2024-03-02 17:06:44 +0800

    feat: 添加 Mirai 适配器的同步消息事件 (#108)

--- packages/alicebot-adapter-mirai/alicebot/adapter/mirai/event/message.py
+++ packages/alicebot-adapter-mirai/alicebot/adapter/mirai/event/message.py
@@ -14,10 +14,9 @@ if TYPE_CHECKING:
     from .. import MiraiAdapter
 
 
-class MessageEvent(MiraiEvent, BaseMessageEvent["MiraiAdapter"]):
-    """消息事件"""
+class MiraiBaseMessageEvent(MiraiEvent, BaseMessageEvent["MiraiAdapter"]):
+    """Mirai 消息事件基类"""
 
-    sender: Union[FriendInfo, GroupMemberInfo, OtherClientSender]
     messageChain: MiraiMessage
 
     @property
@@ -57,6 +56,12 @@ class MessageEvent(MiraiEvent, BaseMessageEvent["MiraiAdapter"]):
         """
         raise NotImplementedError
 
+
+class MessageEvent(MiraiBaseMessageEvent):
+    """消息事件"""
+
+    sender: Union[FriendInfo, GroupMemberInfo, OtherClientSender]
+
     async def is_same_sender(self, other: Self) -> bool:
         """判断自身和另一个事件是否是同一个发送者。
 
@@ -67,6 +72,22 @@ class MessageEvent(MiraiEvent, BaseMessageEvent["MiraiAdapter"]):
             是否是同一个发送者。
         """
         return self.sender.id == other.sender.id
+
+    async def reply(
+        self,
+        message: BuildMessageType[MiraiMessageSegment],
+        quote: bool = False,
+    ) -> Dict[str, Any]:
+        """回复消息。
+
+        Args:
+            message: 回复消息的内容，同 `call_api()` 方法。
+            quote: 引用消息，默认为 `False`。
+
+        Returns:
+            API 请求响应。
+        """
+        raise NotImplementedError
 
 
 class FriendMessage(MessageEvent):
@@ -202,6 +223,131 @@ class OtherClientMessage(MessageEvent):
 
     type: Literal["OtherClientMessage"]
     sender: OtherClientSender
+
+    async def reply(
+        self,
+        message: BuildMessageType[MiraiMessageSegment],
+        quote: bool = False,
+    ) -> Dict[str, Any]:
+        """回复消息。
+
+        Args:
+            message: 回复消息的内容，同 `call_api()` 方法。
+            quote: 引用消息，默认为 `False`。
+
+        Returns:
+            API 请求响应。
+        """
+        raise NotImplementedError
+
+
+class SyncMessage(MiraiBaseMessageEvent):
+    """同步消息"""
+
+    sender: Union[FriendInfo, GroupMemberInfo]
+
+    async def is_same_sender(self, other: Self) -> bool:  # noqa: ARG002
+        """判断自身和另一个事件是否是同一个发送者。
+
+        Args:
+            other: 另一个事件。
+
+        Returns:
+            是否是同一个发送者。
+        """
+        return True
+
+    async def reply(
+        self,
+        message: BuildMessageType[MiraiMessageSegment],
+        quote: bool = False,
+    ) -> Dict[str, Any]:
+        """回复消息。
+
+        Args:
+            message: 回复消息的内容，同 `call_api()` 方法。
+            quote: 引用消息，默认为 `False`。
+
+        Returns:
+            API 请求响应。
+        """
+        raise NotImplementedError
+
+
+class FriendSyncMessage(SyncMessage):
+    """同步好友消息"""
+
+    type: Literal["FriendSyncMessage"]
+    subject: FriendInfo
+
+    async def reply(
+        self,
+        message: BuildMessageType[MiraiMessageSegment],
+        quote: bool = False,
+    ) -> Dict[str, Any]:
+        """回复消息。
+
+        Args:
+            message: 回复消息的内容，同 `call_api()` 方法。
+            quote: 引用消息，默认为 `False`。
+
+        Returns:
+            API 请求响应。
+        """
+        raise NotImplementedError
+
+
+class GroupSyncMessage(SyncMessage):
+    """同步群消息"""
+
+    type: Literal["GroupSyncMessage"]
+    subject: GroupMemberInfo
+
+    async def reply(
+        self,
+        message: BuildMessageType[MiraiMessageSegment],
+        quote: bool = False,
+    ) -> Dict[str, Any]:
+        """回复消息。
+
+        Args:
+            message: 回复消息的内容，同 `call_api()` 方法。
+            quote: 引用消息，默认为 `False`。
+
+        Returns:
+            API 请求响应。
+        """
+        raise NotImplementedError
+
+
+class TempSyncMessage(SyncMessage):
+    """同步群临时消息"""
+
+    type: Literal["TempSyncMessage"]
+    subject: GroupMemberInfo
+
+    async def reply(
+        self,
+        message: BuildMessageType[MiraiMessageSegment],
+        quote: bool = False,
+    ) -> Dict[str, Any]:
+        """回复消息。
+
+        Args:
+            message: 回复消息的内容，同 `call_api()` 方法。
+            quote: 引用消息，默认为 `False`。
+
+        Returns:
+            API 请求响应。
+        """
+        raise NotImplementedError
+
+
+class StrangerSyncMessage(SyncMessage):
+    """同步陌生人消息"""
+
+    type: Literal["StrangerSyncMessage"]
+    subject: FriendInfo
 
     async def reply(
         self,

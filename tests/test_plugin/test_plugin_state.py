0d040ebedf559c6fc2376bfa00e89acae2513632
st1020 <me@st1020.com>
2024-03-18 21:04:24 +0800

    fix: 修复 lint 错误

--- tests/test_plugin/test_plugin_state.py
+++ tests/test_plugin/test_plugin_state.py
@@ -15,8 +15,8 @@ def test_plugin_state(bot: Bot) -> None:
 
     class TestPlugin(Plugin[MessageEvent[Any], int, None]):
         async def handle(self) -> None:
-            if self.state is None:  # pyright: ignore[reportUnnecessaryComparison]
-                self.state = 0
+            if self.state is None:  # pyright: ignore
+                self.state = 0  # pyright: ignore
             self.state += 1
             await self.event.reply(str(self.state))
 

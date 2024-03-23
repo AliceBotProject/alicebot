6fa8d6dd323b7a578be87af2286f9913761d9079
shenyiw <1007313624@qq.com>
2024-03-23 23:09:17 +0800

    refactor: 使用 UnoCSS 重构商店页面

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,205 +1,204 @@
-import fs from "fs";
-import path from "path";
-import { defineConfig, type DefaultTheme } from "vitepress";
+import fs from 'node:fs'
+import path from 'node:path'
+import Unocss from 'unocss/vite'
+import { type DefaultTheme, defineConfig } from 'vitepress'
 
-const baseDir: string = "docs";
+const baseDir: string = 'docs'
 
 export default defineConfig({
-  lang: "zh-CN",
-  title: "AliceBot",
-  description: "简单的 Python 异步多后端聊天机器人框架",
+  lang: 'zh-CN',
+  title: 'AliceBot',
+  description: '简单的 Python 异步多后端聊天机器人框架',
 
   lastUpdated: true,
 
   head: [
-    ["link", { rel: "icon", href: "/logo.png" }],
-    ["link", { rel: "manifest", href: "/manifest.json" }],
-    ["meta", { name: "theme-color", content: "#ffffff" }],
-    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
-    [
-      "meta",
-      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
-    ],
-    ["link", { rel: "apple-touch-icon", href: "/icons/apple-touch-icon.png" }],
-    ["link", { rel: "mask-icon", href: "/icons/safari-pinned-tab.svg" }],
-    [
-      "meta",
-      { name: "msapplication-TileImage", content: "/icons/mstile-150x150.png" },
-    ],
-    ["meta", { name: "msapplication-TileColor", content: "#2d89ef" }],
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/mstile-150x150.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#2d89ef' }],
   ],
 
   themeConfig: {
-    logo: "/logo.png",
+    logo: '/logo.png',
     nav: [
-      { text: "主页", link: "/" },
-      { text: "指南", link: "/guide/" },
-      { text: "API", link: "/api/" },
-      { text: "商店", link: "/store/" },
-      { text: "更新日志", link: "/changelog" },
+      { text: '主页', link: '/' },
+      { text: '指南', link: '/guide/' },
+      { text: 'API', link: '/api/' },
+      { text: '商店', link: '/store/' },
+      { text: '更新日志', link: '/changelog' },
     ],
 
     sidebar: {
-      "/guide/": sidebarGuide(),
-      "/api/": sidebarApi("/api/"),
-      "/dev-api/": sidebarApi("/dev-api/"),
+      '/guide/': sidebarGuide(),
+      '/api/': sidebarApi('/api/'),
+      '/dev-api/': sidebarApi('/dev-api/'),
     },
 
     editLink: {
-      pattern:
-        "https://github.com/AliceBotProject/alicebot/edit/master/docs/:path",
-      text: "在 GitHub 上编辑此页",
+      pattern: 'https://github.com/AliceBotProject/alicebot/edit/master/docs/:path',
+      text: '在 GitHub 上编辑此页',
     },
 
-    lastUpdatedText: "上次更新",
+    lastUpdatedText: '上次更新',
 
     socialLinks: [
-      {
-        icon: "github",
-        link: "https://github.com/AliceBotProject/alicebot",
-      },
+      { icon: 'github', link: 'https://github.com/AliceBotProject/alicebot' },
     ],
 
     footer: {
-      message: "Released under the MIT License.",
-      copyright: "Copyright © 2021-present st1020",
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2021-present st1020',
     },
   },
-});
+
+  vite: {
+    plugins: [
+      Unocss(),
+    ],
+  },
+})
 
 function getSidebarItem(file: string): DefaultTheme.SidebarItem {
-  const content = fs.readFileSync(path.join(baseDir, file), "utf-8");
-  const match = content.match(/^\s*#+\s+(.*)/m);
+  const content = fs.readFileSync(path.join(baseDir, file), 'utf-8')
+  const match = content.match(/^\s*#+\s+(.*)/m)
   if (match === null) {
     return {
-      text: "",
+      text: '',
       link: file,
-    };
+    }
   }
   return {
     text: match[1].trim(),
     link: file,
-  };
+  }
 }
 
 function getSidebarChildrenItems(dir: string): DefaultTheme.SidebarItem[] {
   try {
-    let readme_flag = false;
-    let temp = fs.readdirSync(path.join(baseDir, dir)).filter((name) => {
-      if (name == "index.md") {
-        readme_flag = true;
-        return false;
+    let readme_flag = false
+    const temp = fs.readdirSync(path.join(baseDir, dir)).filter((name) => {
+      if (name === 'index.md') {
+        readme_flag = true
+        return false
       }
       return (
-        !name.startsWith(".") &&
-        fs.statSync(path.join(baseDir, dir, name)).isFile()
-      );
-    });
-    if (readme_flag) temp.unshift("index.md");
-    return temp.map((item) => getSidebarItem(path.join(dir, item)));
-  } catch {
-    return [];
+        !name.startsWith('.')
+        && fs.statSync(path.join(baseDir, dir, name)).isFile()
+      )
+    })
+    if (readme_flag)
+      temp.unshift('index.md')
+    return temp.map(item => getSidebarItem(path.join(dir, item)))
+  }
+  catch {
+    return []
   }
 }
 
 function sidebarGuide(): DefaultTheme.SidebarItem[] {
   return [
     {
-      text: "开始",
+      text: '开始',
       collapsed: false,
       items: [
-        "/guide/index.md",
-        "/guide/quick-start.md",
-        "/guide/basic-config.md",
+        '/guide/index.md',
+        '/guide/quick-start.md',
+        '/guide/basic-config.md',
       ].map(getSidebarItem),
     },
     {
-      text: "基础",
+      text: '基础',
       collapsed: false,
       items: [
-        "/guide/basics/basic-concepts.md",
-        "/guide/basics/load-plugin.md",
-        "/guide/basics/create-plugin.md",
-        "/guide/basics/builtin-message.md",
+        '/guide/basics/basic-concepts.md',
+        '/guide/basics/load-plugin.md',
+        '/guide/basics/create-plugin.md',
+        '/guide/basics/builtin-message.md',
       ].map(getSidebarItem),
     },
     {
-      text: "进阶",
+      text: '进阶',
       collapsed: false,
       items: [
-        "/guide/advanced/event-propagation.md",
-        "/guide/advanced/state-storage.md",
-        "/guide/advanced/plugin-config.md",
-        "/guide/advanced/generic-plugin.md",
-        "/guide/advanced/dependency.md",
-        "/guide/advanced/general-plugin.md",
-        "/guide/advanced/scheduler.md",
-        "/guide/advanced/hook-function.md",
-        "/guide/advanced/hot-reload.md",
+        '/guide/advanced/event-propagation.md',
+        '/guide/advanced/state-storage.md',
+        '/guide/advanced/plugin-config.md',
+        '/guide/advanced/generic-plugin.md',
+        '/guide/advanced/dependency.md',
+        '/guide/advanced/general-plugin.md',
+        '/guide/advanced/scheduler.md',
+        '/guide/advanced/hook-function.md',
+        '/guide/advanced/hot-reload.md',
       ].map(getSidebarItem),
     },
     {
-      text: "协议适配器",
+      text: '协议适配器',
       collapsed: false,
       items: [
-        "/guide/adapters/cqhttp-adapter.md",
-        "/guide/adapters/mirai-adapter.md",
-        "/guide/adapters/dingtalk-adapter.md",
+        '/guide/adapters/cqhttp-adapter.md',
+        '/guide/adapters/mirai-adapter.md',
+        '/guide/adapters/dingtalk-adapter.md',
       ].map(getSidebarItem),
     },
-  ];
+  ]
 }
 
 function sidebarApi(base: string): DefaultTheme.SidebarItem[] {
   return [
     {
-      text: "AliceBot Api Reference",
+      text: 'AliceBot Api Reference',
       items: [
         ...getSidebarChildrenItems(base),
-        getSidebarItem(base + "adapter/index.md"),
+        getSidebarItem(`${base}adapter/index.md`),
         {
-          text: "alicebot.adapter.utils",
-          link: base + "adapter/utils.md",
+          text: 'alicebot.adapter.utils',
+          link: `${base}adapter/utils.md`,
         },
         {
-          text: "alicebot.adapter.cqhttp",
-          link: base + "adapter/cqhttp/index.md",
+          text: 'alicebot.adapter.cqhttp',
+          link: `${base}adapter/cqhttp/index.md`,
           collapsed: true,
-          items: getSidebarChildrenItems(base + "adapter/cqhttp/"),
+          items: getSidebarChildrenItems(`${base}adapter/cqhttp/`),
         },
         {
-          text: "alicebot.adapter.onebot",
-          link: base + "adapter/onebot/index.md",
+          text: 'alicebot.adapter.onebot',
+          link: `${base}adapter/onebot/index.md`,
           collapsed: true,
-          items: getSidebarChildrenItems(base + "adapter/onebot/"),
+          items: getSidebarChildrenItems(`${base}adapter/onebot/`),
         },
         {
-          text: "alicebot.adapter.mirai",
-          link: base + "adapter/mirai/index.md",
+          text: 'alicebot.adapter.mirai',
+          link: `${base}adapter/mirai/index.md`,
           collapsed: true,
           items: [
-            ...getSidebarChildrenItems(base + "adapter/mirai/"),
+            ...getSidebarChildrenItems(`${base}adapter/mirai/`),
             {
-              text: "alicebot.adapter.mirai.event",
-              link: base + "adapter/mirai/event/index.md",
+              text: 'alicebot.adapter.mirai.event',
+              link: `${base}adapter/mirai/event/index.md`,
               collapsed: true,
-              items: getSidebarChildrenItems(base + "adapter/mirai/event/"),
+              items: getSidebarChildrenItems(`${base}adapter/mirai/event/`),
             },
           ],
         },
         {
-          text: "alicebot.adapter.dingtalk",
-          link: base + "adapter/dingtalk/index.md",
+          text: 'alicebot.adapter.dingtalk',
+          link: `${base}adapter/dingtalk/index.md`,
           collapsed: true,
-          items: getSidebarChildrenItems(base + "adapter/dingtalk/"),
+          items: getSidebarChildrenItems(`${base}adapter/dingtalk/`),
         },
         {
-          text: "alicebot.adapter.apscheduler",
-          link: base + "adapter/apscheduler/index.md",
+          text: 'alicebot.adapter.apscheduler',
+          link: `${base}adapter/apscheduler/index.md`,
           collapsed: true,
-          items: getSidebarChildrenItems(base + "adapter/apscheduler/"),
+          items: getSidebarChildrenItems(`${base}adapter/apscheduler/`),
         },
       ],
     },
-  ];
+  ]
 }
--- docs/store/components/Pagination.vue
+++ docs/store/components/Pagination.vue
@@ -1,177 +1,51 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed } from 'vue'
 
-const props = defineProps<{ modelValue: number; pageTotal: number }>();
-const emit = defineEmits<{
-  "update:modelValue": [v: number];
-}>();
+const props = defineProps<{ total: number }>()
+const model = defineModel<number>({ required: true })
 
-const onPrev = () => {
-  if (props.modelValue <= 1) return;
-  emit("update:modelValue", props.modelValue - 1);
-};
-const onNext = () => {
-  if (props.modelValue >= props.pageTotal) return;
-  emit("update:modelValue", props.modelValue + 1);
-};
-const setPageNum = (pageNum: number | string) => {
-  if (typeof pageNum !== "number") return;
-  if (pageNum < 1) return;
-  if (pageNum > props.pageTotal) return;
-  emit("update:modelValue", pageNum);
-};
+function rangeList(start: number, end: number) {
+  return Array.from({ length: end - start + 1 }, (_, i) => i + start)
+}
 
-const genPageArray = (current: number, total: number, size: number) => {
-  let arr: (string | number)[] = [];
-  if (total < size + 2) {
-    arr = Array.from({ length: total }, (_, k) => k + 1);
-  } else if (current < size - 2) {
-    arr = Array.from(
-      (function* gen(i, l) {
-        while (i < l) yield i++;
-      })(1, size - 2 + 1)
-    );
-    arr.push("...");
-    arr.push(total);
-  } else if (total - current < size - 2) {
-    arr.push(1);
-    arr.push("...");
-    arr = arr.concat(
-      Array.from(
-        (function* gen(i, l) {
-          while (i < l) yield i++;
-        })(total - size + 2, total + 1)
-      )
-    );
-  } else {
-    arr.push(1);
-    arr.push("...");
-    arr = arr.concat(
-      Array.from(
-        (function* gen(i, l) {
-          while (i < l) yield i++;
-        })(
-          current - Math.floor((size - 4) / 2),
-          current - Math.floor((size - 4) / 2) + size - 4 + 1
-        )
-      )
-    );
-    arr.push("...");
-    arr.push(total);
-  }
-  return arr;
-};
-
-const pageArrayLg = computed<(number | string)[]>(() => {
-  const current = props.modelValue;
-  const total = props.pageTotal;
-  return genPageArray(current, total, 17);
-});
-const pageArrayMd = computed<(number | string)[]>(() => {
-  const current = props.modelValue;
-  const total = props.pageTotal;
-  return genPageArray(current, total, 10);
-});
-const pageArraySm = computed<(number | string)[]>(() => {
-  const current = props.modelValue;
-  const total = props.pageTotal;
-  return genPageArray(current, total, 6);
-});
+const pageNumberArray = computed<(number | string)[]>(() => {
+  const current = model.value
+  const total = props.total
+  const size = 4 // size hear must greater than 4
+  if (total <= size)
+    return rangeList(1, total)
+  if (current < size - 2)
+    return [...rangeList(1, size - 2), '...', total]
+  if (current > total - size + 3)
+    return [1, '...', ...rangeList(total - size + 3, total)]
+  return [1, '...', ...rangeList(current - Math.floor((size - 4) / 2), current + Math.ceil((size - 4) / 2)), '...', total]
+})
 </script>
 
 <template>
   <div>
-    <div class="container" v-if="props.pageTotal > 1">
-      <div class="paper-item" @click="onPrev" style="display: flex">&lt;</div>
+    <div v-if="props.total > 1" class="my-4 w-full flex justify-center gap-x-2">
       <div
-        v-for="(item, index) in pageArrayLg"
+        class="h-8 w-8 flex cursor-pointer items-center justify-center rounded bg-vp-bg-soft hover:bg-vp-brand-soft hover:text-vp-brand-1"
+        @click="model = Math.max(1, model - 1)"
+      >
+        &lt;
+      </div>
+      <div
+        v-for="(item, index) in pageNumberArray"
         :key="index"
-        @click="setPageNum(item)"
-        class="paper-item paper-lg"
-        :class="{ active: item == props.modelValue }"
+        class="h-8 w-8 flex cursor-pointer items-center justify-center rounded bg-vp-bg-soft hover:bg-vp-brand-soft hover:text-vp-brand-1"
+        :class="item === model ? 'bg-vp-brand-soft text-vp-brand-1' : ''"
+        @click=" () => { if (typeof item === 'number') model = item }"
       >
         {{ item }}
       </div>
       <div
-        v-for="(item, index) in pageArrayMd"
-        :key="index"
-        @click="setPageNum(item)"
-        class="paper-item paper-md"
-        :class="{ active: item == props.modelValue }"
+        class="h-8 w-8 flex cursor-pointer items-center justify-center rounded bg-vp-bg-soft hover:bg-vp-brand-soft hover:text-vp-brand-1"
+        @click="model = Math.min(total, model + 1)"
       >
-        {{ item }}
+        &gt;
       </div>
-      <div
-        v-for="(item, index) in pageArraySm"
-        :key="index"
-        @click="setPageNum(item)"
-        class="paper-item paper-sm"
-        :class="{ active: item == props.modelValue }"
-      >
-        {{ item }}
-      </div>
-      <div class="paper-item" @click="onNext" style="display: flex">></div>
     </div>
   </div>
 </template>
-
-<style scoped>
-.container {
-  margin: 1rem 0;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-}
-
-@media screen and (min-width: 780px) {
-  .paper-lg,
-  .paper-sm {
-    display: none;
-  }
-
-  .paper-md {
-    display: flex;
-  }
-}
-
-@media screen and (min-width: 1024px) {
-  .paper-md,
-  .paper-sm {
-    display: none;
-  }
-
-  .paper-lg {
-    display: flex;
-  }
-}
-
-@media screen and (max-width: 780px) {
-  .paper-lg,
-  .paper-md {
-    display: none;
-  }
-
-  .paper-sm {
-    display: flex;
-  }
-}
-
-.paper-item {
-  height: 2rem;
-  width: 2rem;
-  margin: 0 0.5rem;
-  padding: 0rem 0.25rem;
-  align-items: center;
-  justify-content: center;
-  background-color: var(--vp-c-bg-soft);
-  border-radius: 0.25rem;
-  cursor: pointer;
-}
-
-.paper-item:hover,
-.paper-item.active {
-  background-color: var(--vp-c-brand-soft);
-  --un-text-opacity: 1;
-  color: var(--vp-c-brand-1);
-}
-</style>
